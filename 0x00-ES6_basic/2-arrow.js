4761807cd752929c14648aba35ce607a7b4b2798
Uko Uwatt <uwattuko@gmail.com>
2024-06-25 05:02:09 +0100

    updated task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,3 +1,6 @@
+//Rewrite the following standard function to use ES6’s arrow syntax of 
+//the function add (it will be an anonymous function after)
+
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   

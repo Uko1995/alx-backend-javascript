5287f0debf714e830cce9082213dea0ec359916a
Uko Uwatt <uwattuko@gmail.com>
2024-06-26 16:30:44 +0100

    file corrected

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const = true;
+    const = false;
   }
 
   return [task, task2];

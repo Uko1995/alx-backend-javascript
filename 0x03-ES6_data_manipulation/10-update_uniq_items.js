37f1a1bb298d9e56ddfe32dfc627116e0af58015
Uko Uwatt <uwattuko@gmail.com>
2024-07-02 06:41:38 +0100

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  map.forEach((value, key) => {
+  map.forEach((value, key, map) => {
     if (value === 1) {
       map.set(key, 100);
     }

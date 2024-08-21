511cc8406e172b5a215ec7516bc3457d40dcc786
Uko Uwatt <uwattuko@gmail.com>
2024-08-21 05:04:04 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // create program that interacts in the terminal
 
-console.log("Welcome to Holbertion School, what is your name?");
-process.stdin.on("data", (name) => {
-    console.log(`Your name us : ${name}`);
-    if (!process.stdin.isTTY) {
-        console.log("This important software is closing");
-    }
-    process.exit();
+console.log('Welcome to Holbertion School, what is your name?');
+process.stdin.on('data', (name) => {
+  console.log(`Your name us : ${name}`);
+  if (!process.stdin.isTTY) {
+    console.log('This important software is closing');
+  }
+  process.exit(0);
 });
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
